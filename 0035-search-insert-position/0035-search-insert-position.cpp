25ac5b029995aa6a771e0bf6ccd3ba38029bc81e
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2023-10-07 04:29:36 +0530

    Time: 3 ms (73.15%), Space: 9.9 MB (23.66%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -5,23 +5,17 @@ public:
         int s=0;
         int e=n-1;
         int ans=n;        
-
-        while(s<=e){
-            int mid=s+(e-s)/2;
-
-            if(v[mid]==target){
-                return mid;
-
-            }
-            else if(v[mid]>target){
-                ans=mid;
-                e=mid-1;
-            }
-            else{
-                s=mid+1;
-            }
-        }
-        return ans;
+      while (s<=e){
+          int mid = (s+e)/2;
+          if (v[mid]>=target){
+              ans =mid;
+              e=mid-1;
+          }
+          else{
+              s=mid+1;
+          }
+      }
         
+        return ans;
     }
 };
