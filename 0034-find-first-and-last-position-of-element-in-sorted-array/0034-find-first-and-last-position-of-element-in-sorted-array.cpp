1193cc6c3f362713019d3ec33422d7b9d9c5806c
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2023-10-09 14:05:34 +0530

    Time: 2 ms (92.59%), Space: 13.9 MB (37.79%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,58 +1,33 @@
 class Solution {
 public:
-    int firstOcc(vector<int>&nums,int target){
-        int s=0,ans=-1;
-        int e=nums.size()-1;
-        int  mid=s+(e-s)/2;
-
-        while(s<=e){
-            if(nums[mid]==target){
-                ans=mid;
-                e=mid-1;
-            }
-            else if(nums[mid]<target){
-                s=mid+1;
-            }
-            else{
-                e=mid-1;
-            }
-
-            mid = s+(e-s)/2;
+    int firstOcc(vector<int>&v,int target){
+        int sum=-1;
+        for (int i=0;i<v.size();i++){
+            if(v[i]==target) {
+                sum=i;
+                break;
+            } 
         }
-
-        return ans;
+        return sum;
     }
 
-    int lastOcc(vector<int>&nums, int target){
-        int s=0,ans=-1;
-        int e=nums.size()-1;
-        int  mid=s+(e-s)/2;
-
-        while(s<=e){
-            if(nums[mid]==target){
-                ans=mid;
-                s=mid+1;
-            }
-            else if(nums[mid]<target){
-                s=mid+1;
-            }
-            else{
-                e=mid-1;
-            }
-
-            mid = s+(e-s)/2;
+int num=-1;
+    int lastOcc(vector<int>&v, int target){
+         for (int i=v.size()-1;i>=0;i--){
+            if(v[i]==target) {
+                num=i;;
+                break;
+            } 
         }
+        return num;
 
-        return ans;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int>ans(2);
         ans[0] = firstOcc(nums,target);
         ans[1] = lastOcc(nums,target);
 
-        // ans.push_back(first);
-        // ans.push_back(second);
-
         return ans;
     }
+    
 };
