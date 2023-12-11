8fa6b1e0f4a94d2f02627ba482c9bf07df84825b
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2023-12-11 14:36:14 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (92.60%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -1,27 +1,52 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* swapPairs(ListNode* head) {
+    
+//         struct ListNode *p=head;
+//         while(p!=NULL){
+//             if (p->next==NULL) break;
+//             int data1=p->val;
+//             int data2=p->next->val;
+//             p->val=data2;
+//             p->next->val=data1;
+//             p=p->next;
+//             p=p->next;
+//         }
+//         return head;
+//     }
+// };
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-    
-        struct ListNode *p=head;
-        while(p!=NULL){
-            if (p->next==NULL) break;
-            int data1=p->val;
-            int data2=p->next->val;
-            p->val=data2;
-            p->next->val=data1;
-            p=p->next;
-            p=p->next;
+        ListNode* dummy = new ListNode(0); // Create a dummy node
+        dummy->next = head;
+        ListNode* current = dummy;
+
+        while (current->next != nullptr && current->next->next != nullptr) {
+            ListNode* first = current->next;
+            ListNode* second = current->next->next;
+
+            // Swapping
+            first->next = second->next;
+            current->next = second;
+            current->next->next = first;
+            
+            // Move current two steps forward
+            current = current->next->next;
         }
+
+        head = dummy->next;
+        delete dummy; // Free the dummy node
         return head;
     }
 };
