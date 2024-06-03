8c5b4ba9fda22ff9a8aa1524c8c08f4a3e8c068d
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2024-06-03 16:07:10 +0530

    Time: 23 ms (61.26%), Space: 22.7 MB (68.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,35 +1,3 @@
-// class Solution {
-// public:
-//     vector<vector<int>> merge(vector<vector<int>>& v) {
-//         sort(v.begin(),v.end());
-//      vector<vector<int>>ans;
-//         int store=0;
-//         bool chk=0;
-//         for(auto it:v){
-//             int x= it[0];
-//             int y= it[1];
-//             int z= ++it[0];
-//             if(!chk){
-//             if (y<z){
-//                 ans.push_back({x,y});
-//             }
-//             else{
-//                 store=x;
-//                 chk=true;
-//             }
-//         }
-//             if (chk){
-//                 if (y<z){
-//                     ans.push_back({x,store})
-//                 }
-//             }
-            
-//         }
-//         return ans;
-//     }
-// };
-
-
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
@@ -39,15 +7,28 @@ public:
         sort(intervals.begin(), intervals.end());
 
         vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
+        int storeStart = 0;
+        bool hasOverlap = false;
 
         for (const auto& interval : intervals) {
-            vector<int>& lastInterval = ans.back();
-            if (lastInterval[1] >= interval[0]) {
-                lastInterval[1] = max(lastInterval[1], interval[1]);
-            } 
-            else {
-                ans.push_back(interval);
+            int x = interval[0];
+            int y = interval[1];
+
+            if (!hasOverlap) {
+                if (ans.empty() || ans.back()[1] < x) {
+                    ans.push_back({x, y});
+                } else {
+                    storeStart = ans.back()[0];
+                    ans.back()[1] = max(ans.back()[1], y);
+                    hasOverlap = true;
+                }
+            } else {
+                if (ans.back()[1] < x) {
+                    ans.push_back({x, y});
+                    hasOverlap = false;
+                } else {
+                    ans.back()[1] = max(ans.back()[1], y);
+                }
             }
         }
 
