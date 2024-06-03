be61619b5521ae29adcdd9acae3d569c254a5e86
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2024-06-03 16:10:15 +0530

    Time: 21 ms (73.52%), Space: 22.7 MB (68.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,30 +2,28 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         if (intervals.empty()) return {};
-
-        // Sort intervals based on the starting times
         sort(intervals.begin(), intervals.end());
-
         vector<vector<int>> ans;
-        int storeStart = 0;
-        bool hasOverlap = false;
+        int store = 0;
+        bool chk = false;
 
         for (const auto& interval : intervals) {
             int x = interval[0];
             int y = interval[1];
 
-            if (!hasOverlap) {
+            if (!chk) {
                 if (ans.empty() || ans.back()[1] < x) {
                     ans.push_back({x, y});
                 } else {
-                    storeStart = ans.back()[0];
+                    store = ans.back()[0];
                     ans.back()[1] = max(ans.back()[1], y);
-                    hasOverlap = true;
+                    chk = true;
                 }
-            } else {
+            } 
+            else {
                 if (ans.back()[1] < x) {
                     ans.push_back({x, y});
-                    hasOverlap = false;
+                    chk = false;
                 } else {
                     ans.back()[1] = max(ans.back()[1], y);
                 }
