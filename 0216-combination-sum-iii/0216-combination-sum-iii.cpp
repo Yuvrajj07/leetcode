305301af4469c10f71143e8f24a9c77712f49252
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2024-06-11 22:18:43 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (15.78%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int sum =0;
+    void solve(vector<vector<int>>&ans,vector<int>output,int k,int n,int index){
+        if (output.size()==k && sum ==n){
+            ans.push_back(output);
+            return ;
+        }
+        else if (output.size()==k && sum>n) return ;
+        for (int i =index;i<=9;i++){
+            //  
+        output.push_back(i);
+            sum+=i;
+            solve(ans,output,k,n,i+1);
+            output.pop_back();
+            sum-=i;
+        
+        }
+        
+    }
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>ans;
+        vector<int>output;
+        solve(ans,output,k,n,1);
+        return ans;
+    }
+};
