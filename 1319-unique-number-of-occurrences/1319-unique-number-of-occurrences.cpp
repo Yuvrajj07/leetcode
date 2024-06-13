adeb134ed223e5d8aa678df033e795a53a4dcf7e
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2024-06-14 00:20:09 +0530

    Time: 3 ms (58.8%), Space: 11.2 MB (38.22%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>m;
+        for (int i =0;i<arr.size();i++){
+            m[arr[i]]++;
+        }
+        unordered_set<int>s;
+        for (auto it:m){
+            auto z= s.find(it.second);
+            if(z!=s.end()){
+                return false;
+            }
+            s.insert(it.second);
+        }
+        return true;
+    }
+
+};
