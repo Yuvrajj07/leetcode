7a036a58ebc40a1cd81d28d8d966a0c091e35b66
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2024-08-05 01:37:41 +0530

    Time: 0 ms (100%), Space: 7.4 MB (10.22%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -3,9 +3,7 @@ public:
     int getSum(int a, int b) {
         while(b!=0){
             int carry=a&b;
-            cout<<a<<" ";
             a=a^b;
-            cout<<a<<" ";
             b=carry<<=1;
         }
         return a;
