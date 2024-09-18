d8f09a9b74d5f7615d74a703f2469512106da800
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2024-09-19 01:13:51 +0530

    Time: 3 ms (93.79%), Space: 17 MB (57.24%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -32,35 +32,3 @@ public:
     }
 };
 
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     // Declare the comparison function before using it
-//     static bool cmp(string a, string b) {
-//         if (a.length() != b.length()) {
-//             if (a.length() > b.length()) return false;
-//             else return true;
-//         } else {
-//             if (a > b) return false;
-//             else return true;
-//         }
-//     }
-    
-//     string largestNumber(vector<int>& v) {
-//         sort(v.begin(), v.end(),cmp);
-//         string ans="";
-//         for (int i = 0; i < v.size(); i++) {
-//             cout << v[i] << " ";
-//         }
-
-//         return ans;
-//     }
-// };
