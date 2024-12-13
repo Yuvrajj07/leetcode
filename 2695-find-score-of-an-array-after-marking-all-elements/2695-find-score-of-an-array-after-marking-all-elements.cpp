0d8c3339137e6f7a6bac7a368ad986cd0a02699b
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2024-12-13 18:36:10 +0530

    Time: 233 ms (28.16%), Space: 144.1 MB (24.84%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,106 +1,41 @@
-#include <vector>
-#include <queue>
-#include <utility>
-#include <algorithm>
-#include <numeric>
-using namespace std;
-
 class Solution {
 public:
     long long findScore(vector<int>& nums) {
+        multiset<pair<int, int>> m;
         int n = nums.size();
-        vector<int> processed(n, 0); // To track processed elements
-        long long ans = 0;
 
-        // Min-heap to store {value, index}
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
-
-        // Push all elements with their indices into the heap
+        // Insert all elements with their indices into the multiset
         for (int i = 0; i < n; i++) {
-            minHeap.push({nums[i], i});
+            m.insert({nums[i], i});
         }
 
-        // Process elements in order of smallest value
-        while (!minHeap.empty()) {
-            auto [value, index] = minHeap.top();
-            minHeap.pop();
+        vector<int> v(n, 0); // To track marked elements
+        long long ans = 0;
 
-            // If this element is already processed, skip it
-            if (processed[index]) continue;
+        while (!m.empty()) {
+            // Extract the smallest element from the multiset
+            auto it = m.begin();
+            int value = it->first;
+            int index = it->second;
 
-            // Add value to the score
+            // Remove the element from the multiset
+            m.erase(it);
+
+            // Skip if already processed
+            if (v[index]) continue;
+
+            // Add the value to the score
             ans += value;
 
-            // Mark this element as processed
-            processed[index] = 1;
-
-            // Mark adjacent elements as processed if within bounds
-            if (index > 0) processed[index - 1] = 1;
-            if (index < n - 1) processed[index + 1] = 1;
+            // Mark this element and its neighbors as processed
+            v[index] = 1;
+            if (index > 0) v[index - 1] = 1;
+            if (index < n - 1) v[index + 1] = 1;
         }
 
         return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     long long findScore(vector<int>& nums) {
-//         multiset<int> m(nums.begin(), nums.end());
-//         vector<int> v(nums.size(), 0); // To track marked elements
-//         long long ans = 0;
-
-//         while (!m.empty()) {
-//             int x = *m.begin(); // Get the smallest element
-//             ans += x;
-
-//             for (int i = 0; i < nums.size(); i++) {
-//                 if (nums[i] == x && v[i] == 0) { // Process unmarked element
-//                     m.erase(m.begin()); // Remove the smallest element
-//                     v[i] = 1; // Mark the current element
-
-//                     // Mark and remove the previous element if unmarked
-//                     if (i >= 1 && v[i - 1] == 0) {
-//                         int del1 = nums[i - 1];
-//                         auto it = m.find(del1);
-//                         if (it != m.end()) {
-//                             m.erase(it);
-//                         }
-//                         v[i - 1] = 1;
-//                     }
-
-//                     // Mark and remove the next element if unmarked
-//                     if (i < nums.size() - 1 && v[i + 1] == 0) {
-//                         int del2 = nums[i + 1];
-//                         auto it = m.find(del2);
-//                         if (it != m.end()) {
-//                             m.erase(it);
-//                         }
-//                         v[i + 1] = 1;
-//                     }
-
-//                     break; // Stop searching for `x` in nums
-//                 }
-//             }
-
-//             // Check if all elements are processed
-//             if (accumulate(v.begin(), v.end(), 0) == nums.size()) {
-//                 break;
-//             }
-//         }
-
-//         return ans;
-//     }
-// };
-
 
 
 
