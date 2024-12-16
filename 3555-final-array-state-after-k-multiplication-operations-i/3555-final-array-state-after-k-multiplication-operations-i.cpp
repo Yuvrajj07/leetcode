5edae966195625138c5ee1816ba3e82d92c7934f
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2024-12-16 18:01:19 +0530

    Time: 5 ms (12.85%), Space: 28.9 MB (5.01%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-     int i=0;
-     for(int &a:nums)
-     {
-        pq.push({a,i++});
-     }   
-     while(k--)
-     {
-        pair<int,int>p=pq.top();
-        pq.pop();
-        int a=p.first;
-        int b=p.second;
-        pq.push({a*multiplier,b});
-        nums[b]*=multiplier;
+     multiset<pair<int,int>>m;
+     for (int i =0;i<nums.size();i++){
+        m.insert({nums[i],i});
+     }
+     while (k--){
+        pair<int,int>p1=*m.begin();
+        int x= p1.first;
+        int y= p1.second;
+        m.erase(m.begin());
+        nums[y]=x*multiplier;
+        m.insert({nums[y],y});
      }
      return nums;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+//      priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+//      int i=0;
+//      for(int &a:nums)
+//      {
+//         pq.push({a,i++});
+//      }   
+//      while(k--)
+//      {
+//         pair<int,int>p=pq.top();
+//         pq.pop();
+//         int a=p.first;
+//         int b=p.second;
+//         pq.push({a*multiplier,b});
+//         nums[b]*=multiplier;
+//      }
+//      return nums;
+//     }
+// };
