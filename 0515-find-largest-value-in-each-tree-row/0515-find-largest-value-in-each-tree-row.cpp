4b024d783a79d9892d478b8f896027d763c0a5c7
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2024-12-25 21:44:36 +0530

    Time: 0 ms (100%), Space: 22.9 MB (15.81%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -1,44 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
 class Solution {
 public:
     vector<int> largestValues(TreeNode* root) {
-        vector<int> currentLevel1;   
-         if (root == nullptr) return currentLevel1;
-
-        queue<TreeNode*> q;
+        vector<int>ans;
+        if (root == nullptr) return ans; 
+        queue<TreeNode*>q;
         q.push(root);
-
-        while (!q.empty()) {
-                   vector<int> currentLevel;   
- 
-            int levelSize = q.size();  // Number of nodes at the current level
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode* node = q.front();
+        while (!q.empty()){
+            int mn=INT_MIN;
+            int x= q.size();
+            for (int i =0;i<x;i++){
+                TreeNode *p=q.front();
                 q.pop();
-                currentLevel.push_back(node->val);
-
-                if (node->left != nullptr) {
-                    q.push(node->left);
-                }
-                if (node->right != nullptr) {
-                    q.push(node->right);
-                }
+                int z=p->val;
+                mn=max(mn,z);
+                if (p->right!=NULL) q.push(p->right);
+                if (p->left!=NULL) q.push(p->left);
+                   
             }
-            int x=*max_element(currentLevel.begin(),currentLevel.end());
-          currentLevel1.push_back(x);
+            ans.push_back(mn);
         }
-
-        return  currentLevel1;
+        return ans;
+        
     }
 };
