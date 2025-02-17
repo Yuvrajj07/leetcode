83019361b070ef0e525dab35d4939815110413d9
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-02-17 13:44:26 +0530

    Time: 0 ms (100%), Space: 12.4 MB (75.48%) - LeetHub

--- 0832-binary-tree-pruning/0832-binary-tree-pruning.cpp
+++ 0832-binary-tree-pruning/0832-binary-tree-pruning.cpp
@@ -12,14 +12,12 @@
 class Solution {
 public:
     TreeNode* pruneTree(TreeNode* root) {
-        if (root == NULL) {
-            return NULL;
-        }
-        root->left = pruneTree(root->left);
-        root->right = pruneTree(root->right);
-        if (root->val == 0 && root->left == NULL && root->right == NULL) {
-            return NULL;
-        }
+        if (root==NULL){
+            return root;
+        }   
+        root->left=pruneTree(root->left);
+        root->right=pruneTree(root->right);
+    if (root->val==0 && root->right==NULL && root->left==NULL) return NULL;
         
         return root;
     }
