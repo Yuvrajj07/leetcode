e950c870af26a8e78d377ed5305eb04d01a3dbd4
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-03-03 17:53:57 +0530

    Time: 4 ms (80.6%), Space: 133.5 MB (41.94%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,53 +1,22 @@
-// class Solution {
-// public:
-//     vector<int> pivotArray(vector<int>& nums, int pivot) {
-//         int pos=-1;
-//         for (int i =0;i<nums.size();i++){
-//             if (nums[i]==pivot){
-//                 pos=i;
-//                 break;
-//             } 
-//         }
-//         vector<int>ans(nums.size());
-//         ans[pos]=pivot;
-//         int i =0,j=pos+1;
-//         for (int i =0;i<nums.size();i++){
-//             if (nums[i]<pivot){
-//                 ans[i]=nums[i];
-//                 i++;
-//             }
-//             else {
-//                 ans[j]=nums[i];
-//                 j++;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-
-/*
-    ...................................approach : take three vectors........................
-    
-    1)- left vector :for samller values
-    2)- mid vector :for equal  values
-    3)- right vector :for larger values
-*/
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> left, right, mid;
+        vector<int> ans;
         
+        // First, add elements smaller than pivot
         for (int num : nums) {
-            if (num < pivot) left.push_back(num);
-            else if (num > pivot) right.push_back(num);
-            else mid.push_back(num);
+            if (num < pivot) ans.push_back(num);
         }
         
-        vector<int> ans;
-        ans.insert(ans.end(), left.begin(), left.end());
-        ans.insert(ans.end(), mid.begin(), mid.end());
-        ans.insert(ans.end(), right.begin(), right.end());
+        // Then, add elements equal to pivot
+        for (int num : nums) {
+            if (num == pivot) ans.push_back(num);
+        }
+        
+        // Finally, add elements greater than pivot
+        for (int num : nums) {
+            if (num > pivot) ans.push_back(num);
+        }
         
         return ans;
     }
