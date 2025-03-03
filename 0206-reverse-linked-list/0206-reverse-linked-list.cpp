a7ab2fb3935b0e8afab8112a01d8f2eaac6c71cd
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-03-03 18:59:21 +0530

    Time: 0 ms (100%), Space: 13.2 MB (90.71%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,16 +8,24 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+
+
+
+
 class Solution {
 public:
+    ListNode *reverse(ListNode *curr,ListNode *prev){
+        if (curr==NULL) return prev;
+        ListNode *forw=curr->next;
+        curr->next=prev;
+        prev=curr;
+        return reverse(forw,prev);
+        
+    }
     ListNode* reverseList(ListNode* head) {
-        ListNode *curr =head,*prev=NULL,*forw=NULL;
-        while (curr!=NULL){
-            forw=curr->next;
-            curr->next =prev;
-            prev=curr;
-            curr=forw;
-        }
-        return prev;
+        ListNode *curr=head,*prev=NULL, *forw=NULL;
+        return reverse(curr,NULL);
     }
 };
