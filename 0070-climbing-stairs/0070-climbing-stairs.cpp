75f8fbe265e410c4f8d1f1093eb8a5d70eecafc7
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-03-19 18:18:27 +0530

    Time: 0 ms (100%), Space: 8.6 MB (34.02%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,87 @@
+
+
+//.................... USING DYNAMIC PROGRAMING................
+
+//.....................TOP DOWN APPPROACH......................
+
+
+
+
+
 class Solution {
 public:
-    int climbStairs(int n) {
-        int a=0,b=1,c=0;
-        for (int i =1;i<=n;i++){
-           c= a+b;
-            a=b;
-            b=c;
-
-        }
-       return c;
-
+    int ans(int n,vector<int>&dp){
+            if( n==1 || n==2){
+                return n;
+            }
+            if(dp[n-1]!=-1) return dp[n-1];
+            return dp[n-1]=ans(n-1,dp)+ans(n-2,dp);
+            return dp[n];
     }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);  
+            return ans(n,dp);    
+    }    
 };
+
+
+
+
+// class Solution {
+// public:
+//     int ans(int n,vector<int>&dp){
+//         if (n==1 || n==2){
+//             return n;
+//         }
+//         if (dp[n]!=-1){
+//             return dp[n];
+//         }
+//         // return 
+//             dp[n]=ans(n-1,dp)+ans(n-2,dp);
+//             return dp[n];
+//     }
+//     int climbStairs(int n) {
+//         vector<int>dp(n+1,-1);  
+//             return ans(n,dp);    
+//     }    
+// };
+
+
+
+
+
+//..............BOTTOM UP-(TABULATION METHOD)...................
+
+// class Solution {
+// public:
+
+//     int climbStairs(int n) {
+//         vector<int>dp(n+1,-1);
+//         if (n==1 || n==2 ) return n;
+//         dp[1]=1;
+//         dp[2]=2;
+//         for (int i =3;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }    
+// };
+
+
+// ....................USING RECURSION -(TLE)................... 
+// class Solution {
+// public:
+// int ans (int n,int t){
+//     if (n==t){
+//         return 1;
+//     }
+//     if (n>t){
+//         return 0;
+//     }
+//     return (ans((n+1),t) + ans((n+2),t));
+// }
+//     int climbStairs(int n) {
+//         return ans(0,n);
+        
+//     }
+// };
