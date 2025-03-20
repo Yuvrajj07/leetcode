f245cf2100acdb1224ff2acb2372efe3f2cfd473
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-03-20 09:32:45 +0530

    Time: 0 ms (100%), Space: 13.1 MB (6.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,13 +1,22 @@
 class Solution {
-public:
+public: 
+    int solve(vector<int>v,int n,vector<int>&dp){
+        if (n<=0)  return 0;
+        if (dp[n]!=-1)  return dp[n];
+       
+        int loot=solve(v,n-2,dp)+v[n-1];
+        int nloot=solve(v,n-1,dp);
+        return  dp[n]=max(loot,nloot); 
+    } 
     int rob(vector<int>& nums) {
         int n= nums.size();
-        vector<int>dp(n+1);
-        dp[0]=0;
-        dp[1]=nums[0];
-        for (int i =2;i<=n;i++){
-            dp[i]=max(nums[i-1]+dp[i-2],dp[i-1]);
-        }
-        return dp[n];
+        vector<int>dp(n+1,-1);
+        return solve(nums,n,dp);
+        // dp[0]=0;
+        // dp[1]=nums[0];
+        // for (int i =2;i<=n;i++){
+        //     dp[i]=max(nums[i-1]+dp[i-2],dp[i-1]);
+        // }
+        // return dp[n];
     }
 };
