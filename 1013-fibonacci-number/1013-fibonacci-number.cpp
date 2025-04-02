5b52d109ab717467ba0ecb2aa838f1753e0bdc80
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-04-02 21:23:21 +0530

    Time: 0 ms (100%), Space: 7.5 MB (99.98%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,9 +1,35 @@
 class Solution {
-public:
-    int fib(int n) {
-        if (n==0 || n==1){
-            return n;
+
+// USING DYNAMIC PROGRAMING
+//..............BOTTOM UP-(TABULATION METHOD)...................  
+    public:
+     
+        int fib(int n) {
+        if (n==0 || n==1) return n;
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        for (int i =2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return fib(n-1)+fib(n-2);
+            return  dp[n];
     }
 };
+
+//.............TOP DOWN .................
+// public:
+// int fib(int n, vector<int>&dp){
+//     if (n==0 || n==1){
+//         return n;
+//     }
+//     if (dp[n]!=-1){
+//         return dp[n];
+//     }
+//     dp[n]=fib(n-1)+fib(n-2);
+//     return dp[n];
+// }
+//     int fib(int n) {
+//         vector<int>dp(n+1,-1);
+//         return fib(n,dp);
+//     }
+// };
