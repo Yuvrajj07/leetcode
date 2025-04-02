cc734c0bf65b792bdf0e508dde3839935558e857
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-04-02 19:27:07 +0530

    Time: 552 ms (9.09%), Space: 55.9 MB (5.71%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool solve(int i ,int n,vector<int>&v,vector<int>&dp){
+        if (i==n-1) return true;
+        if (dp[i]!=-1) return dp[i];
+        for (int jump=1;jump<=v[i];jump++){
+            if (i+jump<n &&  solve(i+jump,n,v,dp)){
+                return dp[i]=true;
+            }
+        }
+        return dp[i]=false;
+    }
+    bool canJump(vector<int>& v) {
+        int n=v.size();
+        vector<int>dp(n,-1);
+        return  solve (0,n,v,dp);
+    }
+};
+
