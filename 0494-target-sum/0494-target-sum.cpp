b3be6ef5d0bc2963f20d0aa815cdb88f74d48564
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-04-08 09:43:56 +0530

    Time: 412 ms (28.23%), Space: 188 MB (5.56%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -21,75 +21,54 @@
 // };
 
 
-// class Solution {
-// public:
-//     int findTargetSumWays(vector<int>& nums, int target) {
-//         unordered_map<string, int> memo; // Memoization to store intermediate results
-//         return calc(nums, 0, 0, target, memo);
-//     }
-// private:
-//     int calc(vector<int>& nums, int sum, int idx, int target, unordered_map<string, int>& memo) {
-//         if (idx == nums.size()) {
-//             return (sum == target) ? 1 : 0;
-//         }
-
-//         // Create a unique key for the current state
-//         string key = to_string(idx) + "," + to_string(sum);
-
-//         // Check if the result for this state is already computed
-//         if (memo.find(key) != memo.end()) {
-//             return memo[key];
-//         }
-
-//         // Recursive cases: Add and subtract the current number
-//         int add = calc(nums, sum + nums[idx], idx + 1, target, memo);
-//         int subtract = calc(nums, sum - nums[idx], idx + 1, target, memo);
-
-//         // Store the result in the memo table
-//         memo[key] = add + subtract;
-//         return memo[key];
-//     }
-// };
-
 class Solution {
 public:
-    int totalSum;
-
-    int findTargetSumWays(vector<int>& nums, int target) {
-        totalSum = accumulate(nums.begin(), nums.end(), 0);
-        vector<vector<int>> memo(nums.size(), vector<int>(2 * totalSum + 1, numeric_limits<int>::min()));
-        return calculateWays(nums, 0, 0, target, memo);
-    }
-
-    int calculateWays(vector<int>& nums, int currentIndex, int currentSum,
-                      int target, vector<vector<int>>& memo) {
-        if (currentIndex == nums.size()) {
-            // Check if the current sum matches the target
-            if (currentSum == target) {
-                return 1;
-            } else {
-                return 0;
-            }
-        } else {
-            // Check if the result is already computed
-            if (memo[currentIndex][currentSum + totalSum] !=
-                numeric_limits<int>::min()) {
-                return memo[currentIndex][currentSum + totalSum];
-            }
-            // Calculate ways by adding the current number
-            int add =
-                calculateWays(nums, currentIndex + 1,
-                              currentSum + nums[currentIndex], target, memo);
-
-            // Calculate ways by subtracting the current number
-            int subtract =
-                calculateWays(nums, currentIndex + 1,
-                              currentSum - nums[currentIndex], target, memo);
-
-            // Store the result in memoization table
-            memo[currentIndex][currentSum + totalSum] = add + subtract;
-
-            return memo[currentIndex][currentSum + totalSum];
+    
+    int solve(int i,int sum ,int target ,vector<int>v,vector<vector<int>>&dp,int mx){
+        if (i==v.size()){
+            if (sum ==target) return 1;
+            else return 0;
         }
+        if (dp[i][sum+mx]!=-1) return dp[i][sum+mx];
+        int include=solve(i+1,sum+v[i],target,v,dp,mx);
+        int exclude = solve(i+1,sum-v[i],target,v,dp,mx);
+        return  dp[i][sum+mx]= include +exclude;     
+        }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        int mx= accumulate(nums.begin(),nums.end(),0);
+        vector<vector<int>>dp(n+1,vector<int>(2*mx+1,-1));
+        return solve(0,0,target,nums,dp,mx);
     }
+
 };
+// class Solution {
+// public:
+//     int totalSum;
+
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         totalSum = accumulate(nums.begin(), nums.end(), 0);
+//         vector<vector<int>> memo(nums.size(), vector<int>(2 * totalSum + 1, numeric_limits<int>::min()));
+//         return calculateWays(nums, 0, 0, target, memo);
+//     }
+
+//     int calculateWays(vector<int>& nums, int currentIndex, int currentSum,int target,vector<vector<int>>& memo) {
+//         if (currentIndex == nums.size()) {
+//             if (currentSum == target) {
+//                 return 1;
+//             } 
+//             else {
+//                 return 0;
+//             }
+//         } 
+//         else {
+//             if (memo[currentIndex][currentSum + totalSum] !=numeric_limits<int>::min()) {
+//                 return memo[currentIndex][currentSum + totalSum];
+//             }
+//             int add =calculateWays(nums, currentIndex + 1,currentSum + nums[currentIndex], target, memo);
+//             int subtract =calculateWays(nums, currentIndex + 1,currentSum - nums[currentIndex], target, memo);
+//             memo[currentIndex][currentSum + totalSum] = add + subtract;
+//             return memo[currentIndex][currentSum + totalSum];
+//         }
+//     }
+// };
