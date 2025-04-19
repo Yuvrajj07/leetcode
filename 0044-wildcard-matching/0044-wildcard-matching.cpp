0e89af16a50dc1dc77f9d0b1ec8cbf027fdf185a
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-04-19 20:51:09 +0530

    Time: 36 ms (34.73%), Space: 31.9 MB (8.7%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool solve(int i, int j, string &s, string &p, vector<vector<int>>& dp) {
+        if (i == 0 && j == 0) return true;
+        if (j == 0 && i > 0) return false;
+        if (i == 0 && j > 0) {
+            for (int k = 1; k <= j; k++) {
+                if (p[k - 1] != '*') return false;
+            }
+            return true;
+        }
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+            return dp[i][j] = solve(i - 1, j - 1, s, p, dp);
+        else if (p[j - 1] == '*')
+            return dp[i][j] = solve(i - 1, j, s, p, dp) || solve(i, j - 1, s, p, dp);
+
+        return dp[i][j] = false;
+    }
+
+    bool isMatch(string s, string p) {
+        int n = s.size(), m = p.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        return solve(n, m, s, p, dp);
+    }
+};
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool solve(int i,int j ,string s,string p,vector<vector<int>>&dp){
+//         if (i<0 && j<0) return true;
+//          if (i>=0 && j<0) return false;
+//         if (i<0 && j>=0){
+//             for (int x=j;x>=0;x--){
+//                 if (p[x]!='*') return false;
+//             }
+//             return true;
+//         }
+//         if (dp[i][j]!=-1) return dp[i][j];
+//         if (s[i-1]==p[j-1]  || p[j-1]=='?' ) return dp[i][j]= solve(i-1,j-1,s,p,dp);
+//         else if (p[j-1]=='*') return dp[i][j]= solve(i-1,j,s,p ,dp) ||  solve(i,j-1,s,p,dp);
+//         return dp[i][j]= false;
+//     }
+//     bool isMatch(string s, string p) {
+//         int n= s.length(),m=p.length();
+//         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+//         return solve(n,m,s,p,dp);
+        
+//     }
+// };
