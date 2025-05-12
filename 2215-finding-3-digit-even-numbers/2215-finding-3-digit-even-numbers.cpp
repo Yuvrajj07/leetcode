7d69f76487916b744755f0ff865ad7dfffaf2757
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-05-12 14:46:57 +0530

    Time: 901 ms (5.04%), Space: 15.2 MB (23.98%) - LeetHub

---
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -0,0 +1,65 @@
+// class Solution {
+// public:
+//     vector<int> findEvenNumbers(vector<int>&v) {
+//         vector<int>op;
+//         set<int>s;
+//         int n=v.size();
+//         string ans="";
+//         for (int i=0;i<n;i++){
+//             if (v[i]!=0){
+//             ans.push_back(('0'+v[i]));
+//             for (int j=i+1;j<n;j++){
+//                 ans.push_back(('0'+v[j]));
+//                 for (int k=j+1;k<n;k++){
+//                     if (v[k]%2==0){
+//                         ans.push_back(('0'+v[k]));
+//                         if (ans.size()==3){
+//                              s.insert(stoi(ans));
+//                         }
+//                         ans.pop_back();
+//                     }
+//                 }
+//                 ans.pop_back();
+//               }
+//               ans.pop_back();
+//             }
+//         }
+//         return vector<int>(s.begin(),s.end());
+//     }
+// };
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& v) {
+        set<int> s; // to avoid duplicates
+        int n = v.size();
+        string ans = "";
+
+        for (int i = 0; i < n; i++) {
+            if (v[i] != 0) { // first digit can't be 0
+                ans.push_back('0' + v[i]); //
+                for (int j = 0; j < n; j++) {
+                    if (j == i) continue;
+                    ans.push_back('0' + v[j]); 
+                    for (int k = 0; k < n; k++) {
+                        if (k == i || k == j) continue;
+                        if (v[k] % 2 == 0) { 
+                            ans.push_back('0' + v[k]);
+                            if (ans.size() == 3) {
+                                try {
+                                    s.insert(stoi(ans)); // safe with valid string
+                                } catch (...) {
+                                    // just in case, skip invalid
+                                }
+                            }
+                            ans.pop_back();
+                        }
+                    }
+                    ans.pop_back();
+                }
+                ans.pop_back();
+            }
+        }
+
+        return vector<int>(s.begin(), s.end());
+    }
+};
