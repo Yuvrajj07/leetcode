cd8e612645dccc987c5895a84683ff60a8e692ef
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-05-12 18:06:22 +0530

    Time: 881 ms (5.04%), Space: 15.3 MB (23.98%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& v) {
-        set<int> unique;  // use set to avoid duplicates
+        set<int> s; // to avoid duplicates
         int n = v.size();
-        
+        string ans = "";
+
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    if (i != j && j != k && i != k) {
-                        // build the number using 3 digits
-                        int a = v[i], b = v[j], c = v[k];
-
-                        // first digit should not be 0
-                        if (a == 0) continue;
-
-                        // last digit should be even
-                        if (c % 2 != 0) continue;
-
-                        int num = a * 100 + b * 10 + c;
-                        unique.insert(num);
+            if (v[i] != 0) { // first digit can't be 0
+                ans.push_back('0' + v[i]); //
+                for (int j = 0; j < n; j++) {
+                    if (j == i) continue;
+                    ans.push_back('0' + v[j]); 
+                    for (int k = 0; k < n; k++) {
+                        if (k == i || k == j) continue;
+                        if (v[k] % 2 == 0) { 
+                            ans.push_back('0' + v[k]);
+                            if (ans.size() == 3) {
+                                    s.insert(stoi(ans)); // safe with valid string
+                            }
+                            ans.pop_back();
+                        }
                     }
+                    ans.pop_back();
                 }
+                ans.pop_back();
             }
         }
 
-        return vector<int>(unique.begin(), unique.end());
+        return vector<int>(s.begin(), s.end());
     }
 };
