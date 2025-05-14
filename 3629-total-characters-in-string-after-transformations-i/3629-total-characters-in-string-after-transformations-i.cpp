f111ce3662d4b44d8383f82645f53befb633faff
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-05-14 15:02:18 +0530

    Time: 87 ms (78.54%), Space: 19 MB (96.59%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
@@ -1,26 +1,58 @@
+// class Solution {
+// public:
+//     int lengthAfterTransformations(string s, int t) {
+//         int n=s.length();
+//         for (int i =0;i<t;i++){
+//             for (int j =0;j<s.length();j++){
+//                 char c=s[j];
+//                 s.erase(s.begin()+j,s.begin()+j+1);
+//                 if (c=='z') {
+//                     s.push_back('a');
+//                     s.push_back('b');
+//                 }
+//                 else {
+//                     c++;
+//                     s.push_back(c);
+//                 }
+//             }
+
+//         }
+//         cout<<s<<endl;
+//         return s.length();
+//     }
+
+// };
+
+
 class Solution {
 public:
     int lengthAfterTransformations(string s, int t) {
-        vector<int> cnt(26);
-        for (char ch : s) {
-            ++cnt[ch - 'a'];
+        vector<int>v(26,0);
+        for(int i=0;i<s.length();i++){
+            int x= s[i]-'a';
+            v[x]++;
         }
-        for (int round = 0; round < t; ++round) {
-            vector<int> nxt(26);
-            nxt[0] = cnt[25];
-            nxt[1] = (cnt[25] + cnt[0]) % mod;
-            for (int i = 2; i < 26; ++i) {
-                nxt[i] = cnt[i - 1];
-            }
-            cnt = move(nxt);
+        int mod=1e9+7;
+        while (t--){
+            int prev=0,z=0;
+            int curr;
+            for (int i =0;i<26;i++){
+                    curr=v[i];
+                    if (i==25) z=curr;
+                    v[i]=prev;
+                    prev=curr;
+                }
+                z=z%mod;
+                v[0]+=z;
+                v[1]+=z;
         }
-        int ans = 0;
-        for (int i = 0; i < 26; ++i) {
-            ans = (ans + cnt[i]) % mod;
+        int ans=0;
+        for (auto it:v){
+            cout<<it<<" ";
+            ans+=(it%mod);
+            ans%=mod;
         }
         return ans;
     }
-
-private:
-    static constexpr int mod = 1000000007;
 };
+
