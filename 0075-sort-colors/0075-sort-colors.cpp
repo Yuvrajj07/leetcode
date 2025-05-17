ec89572c1aa42eab0fbb525b33b2c8f988a70678
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-05-17 21:41:43 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.91%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& v) {
-        int z=0,o=0,t=0;
-        for (int i =0;i<v.size();i++){
-            if (v[i]==1) o++;
-            else if (v[i]==2) t++;
-            else z++;
+        int o=0,z=0,t=0;
+        for (auto it:v){
+            if(it==1) o++;
+            else if(it==2) t++;
+            else z++;           
         }
-        // cout<<o<<t<<z<<endl;
-         for (int i =0;i<v.size();i++){
-        if (i<z) v[i]=0;
-        else if (i>=z && i<o+z)  v[i]=1;
-        else  v[i]=2;
-    }
-
+        int i =0;
+        while (i<v.size()){
+            if (z>0){
+                v[i]=0;
+                z--;
+            }
+            else if (o>0){
+                v[i]=1;
+                o--;
+            }
+            else {
+                v[i]=2;
+            }
+            i++;
+        }
+        // return v;
     }
 };
