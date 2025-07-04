b934d75d1bd4beada5366b8a28afb2e3a8f7ef16
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-07-04 13:47:53 +0530

    Time: 0 ms (100%), Space: 10.5 MB (87.52%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
     vector<int>t;
     vector<bool>used;
-    void solve(vector<int>&nums,vector<vector<int>>&ans){
-        if (t.size()==nums.size()) {
-            ans.push_back(t);
-            return ;
+
+    void solve(vector<int>& nums ,set<vector<int>>&ans){
+        if (t.size()==nums.size()){
+            ans.insert(t);
+            return;
         }
-        for (int i =0;i<nums.size();i++){
-            if (used[i]) continue;
-            used [i]=true;
+        for (int i=0;i<nums.size();i++){
+            if (used[i]) continue ;
+            used[i]=true;
             t.push_back(nums[i]);
             solve(nums,ans);
             t.pop_back();
             used[i]=false;
-        }        
+        }
     }
+
+
+
     vector<vector<int>> permute(vector<int>& nums) {
-     used =vector<bool>(nums.size(),false);
-     vector<vector<int>>ans;
-     solve(nums,ans);
-     return ans;   
+        used=vector<bool>(nums.size(),false);
+        set<vector<int>>ans;
+        solve (nums,ans);
+        return vector<vector<int>>(ans.begin(),ans.end());
     }
 };
