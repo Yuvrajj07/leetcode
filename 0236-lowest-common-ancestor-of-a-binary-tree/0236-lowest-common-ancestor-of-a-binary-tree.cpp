04b104cb476d11e608fe7a543a73bbf80c9f4711
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-07-06 23:44:20 +0530

    Time: 12 ms (64.26%), Space: 17.2 MB (97.79%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,28 +1,47 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == nullptr || root == p || root == q) {
-            return root;
+        if (root==NULL) return NULL;
+        if (root==p || root==q) return root;
+        TreeNode *left= lowestCommonAncestor(root->left,p,q);
+        TreeNode *right= lowestCommonAncestor(root->right,p,q);
+        if (left!=NULL && right !=NULL) return root;
+        else {
+            if (left) return left;
+            else return right;
         }
+    
 
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-
-        if (left == nullptr) {
-            return right;
-        } else if (right == nullptr) {
-            return left;
-        } else {
-            return root;
-        }
     }
 };
+
+
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if (root == nullptr || root == p || root == q) {
+//             return root;
+//         }
+
+//         TreeNode* left = lowestCommonAncestor(root->left, p, q);
+//         TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+//         if (left == nullptr) {
+//             return right;
+//         } else if (right == nullptr) {
+//             return left;
+//         } else {
+//             return root;
+//         }
+//     }
+// };
