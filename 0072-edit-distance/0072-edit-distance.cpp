4dbd7ddbe14e4699b0976f6b376562dd6c0c8573
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-07-18 11:24:04 +0530

    Time: 307 ms (5%), Space: 401.4 MB (5.61%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int solve(int n1,int n2,string s1,string s2,vector<vector<int>>&dp){
-        // if (n1<=0 || n2<=0) return max(n1,n2);
-        if (n1<=0) return n2;
-        if (n2<=0) return n1;
+        if (n1<=0 || n2<=0) return max(n1,n2);
+        // if (n1<=0) return n2;
+        // if (n2<=0) return n1;
         if (dp[n1][n2]!=-1) return dp[n1][n2];
         if (s1[n1-1]==s2[n2-1]) return solve (n1-1,n2-1,s1,s2,dp);
         int  insert=1+solve (n1,n2-1,s1,s2,dp);
