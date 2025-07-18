d085aa0b8e7caee95970578c2e1d65d00f6f7e5a
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-07-18 11:53:20 +0530

    Time: 7 ms (59.45%), Space: 13.2 MB (46.15%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    int solve(int n1,int n2,string s1,string s2,vector<vector<int>>&dp){
-        if (n1<=0 || n2<=0) return max(n1,n2);
-        // if (n1<=0) return n2;
-        // if (n2<=0) return n1;
-        if (dp[n1][n2]!=-1) return dp[n1][n2];
-        if (s1[n1-1]==s2[n2-1]) return solve (n1-1,n2-1,s1,s2,dp);
-        int  insert=1+solve (n1,n2-1,s1,s2,dp);
-        int  del =1+solve (n1-1,n2,s1,s2,dp);
-        int  replace=1+solve (n1-1,n2-1,s1,s2,dp);
-        return dp[n1][n2]=min(insert,min(del,replace));
-        
-    }
     int minDistance(string s1, string s2) {
-        int n1=s1.length(),n2=s2.length();
-        vector<vector<int>>dp(n1+1,vector<int>(n2+1,-1));
-        return  solve(n1,n2,s1,s2,dp);
+        
+        int n=s1.length(),m=s2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        for (int i=0;i<=n;i++){
+            for (int j=0;j<=m;j++){
+                if (i==0){
+                    dp[i][j]=j;
+                }
+                else if (j==0){
+                    dp[i][j]=i;
+                }
+            }
         }
+        for (int i =1;i<=n;i++){
+            for (int j=1;j<=m;j++){
+                if (s1[i-1]==s2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
 };
