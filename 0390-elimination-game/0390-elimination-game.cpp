d64a852cf1e60416ef638a5be21a6c55b2fb33bd
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-07-23 22:58:21 +0530

    Time: 0 ms (100%), Space: 9.6 MB (45.14%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.cpp
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
 
-int lastRemaining(int n) {
-    int head = 1,step = 1;
-    bool left = true;
-    int remaining = n;
-    while (remaining > 1) {
-        if (left || remaining % 2 == 1) {
-            head += step;
-        }
-        step *= 2;
-        remaining /= 2;
-        left = !left;
-    }
+int helper(int n, bool left) {
+    if (n == 1) return 1;
 
-    return head;
+    if (left) {
+        return 2 * helper(n / 2, false);
+    } else {
+        return 2 * helper(n / 2, true) - 1 + (n % 2);
+    }
 }
+
+int lastRemaining(int n) {
+    return helper(n, true); // Start from left to right
+}
+// int lastRemaining(int n) {
+//     int head = 1,step = 1;
+//     bool left = true;
+//     int remaining = n;
+//     while (remaining > 1) {
+//         if (left || remaining % 2 == 1) {
+//             head += step;
+//         }
+//         step *= 2;
+//         remaining /= 2;
+//         left = !left;
+//     }
+
+//     return head;
+// }
 };
