d16b139c132f83979b6a9f78fd508d10df7fbb5b
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-08-15 21:11:01 +0530

    Time: 0 ms (100%), Space: 7.9 MB (18.14%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        long long  ans=1;
+        while (ans<n){
+            ans=ans<<2;
+            
+        }
+        return ans==n;
+    }
+};
