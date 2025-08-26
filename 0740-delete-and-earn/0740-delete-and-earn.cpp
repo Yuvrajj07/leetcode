ed17ff273dea8ffa8782d316ba328874a4292599
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-08-26 14:34:12 +0530

    Time: 212 ms (5.04%), Space: 90.5 MB (5.01%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,49 +1,26 @@
-// class Solution {
-// public:
-// vector<int>dp;
-//   int solve(int i, vector<int>& v) {
-//         if (i <= 0) return 0; // base case
-//         if (dp[i] != -1) return dp[i]; 
-//         return dp[i] = max(v[i] + solve(i-2, v), solve(i-1, v));
-//     }
-
-//     int deleteAndEarn(vector<int>& nums) {
-//         unordered_map<int,int>m;
-//         for (auto it:nums){
-//             m[it]++;
-//         }
-//         vector<int>v(20001,0);
-//         for (int i =1;i<=20001;i++){
-//             v[i]=i*m[i];
-//         }
-        
-//         dp.assign(20001, -1);
-//         return solve(20000, v);
-//     }
-// };
-
 class Solution {
 public:
-    vector<int> dp;
-
-    int solve(int i, vector<int>& v) {
+vector<int>dp;
+  int solve(int i, vector<int>& v) {
         if (i <= 0) return 0; // base case
-        if (dp[i] != -1) return dp[i];
+        if (dp[i] != -1) return dp[i]; 
         return dp[i] = max(v[i] + solve(i-2, v), solve(i-1, v));
     }
 
     int deleteAndEarn(vector<int>& nums) {
-        unordered_map<int,int> m;
-        for (auto it : nums) {
+        unordered_map<int,int>m;
+        for (auto it:nums){
             m[it]++;
         }
-
-        vector<int> v(20001, 0);
-        for (int i = 1; i <= 20000; i++) {
-            v[i] = i * m[i];
+        vector<int>v(20001,0);
+        for (int i =1;i<=20000;i++){
+            v[i]=i*m[i];
         }
-
+        
         dp.assign(20001, -1);
         return solve(20000, v);
     }
 };
+
+
+// calculate the sum of every element and store it in an array . and find the max  sum of entire array by not taking the consecutive elements  
