3ec88c65cfdc5217ee9f4f0d0e488d87b8405941
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2025-08-26 14:27:10 +0530

    Time: 203 ms (5.04%), Space: 90.5 MB (5.01%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,49 @@
+// class Solution {
+// public:
+// vector<int>dp;
+//   int solve(int i, vector<int>& v) {
+//         if (i <= 0) return 0; // base case
+//         if (dp[i] != -1) return dp[i]; 
+//         return dp[i] = max(v[i] + solve(i-2, v), solve(i-1, v));
+//     }
+
+//     int deleteAndEarn(vector<int>& nums) {
+//         unordered_map<int,int>m;
+//         for (auto it:nums){
+//             m[it]++;
+//         }
+//         vector<int>v(20001,0);
+//         for (int i =1;i<=20001;i++){
+//             v[i]=i*m[i];
+//         }
+        
+//         dp.assign(20001, -1);
+//         return solve(20000, v);
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> dp;
+
+    int solve(int i, vector<int>& v) {
+        if (i <= 0) return 0; // base case
+        if (dp[i] != -1) return dp[i];
+        return dp[i] = max(v[i] + solve(i-2, v), solve(i-1, v));
+    }
+
+    int deleteAndEarn(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for (auto it : nums) {
+            m[it]++;
+        }
+
+        vector<int> v(20001, 0);
+        for (int i = 1; i <= 20000; i++) {
+            v[i] = i * m[i];
+        }
+
+        dp.assign(20001, -1);
+        return solve(20000, v);
+    }
+};
