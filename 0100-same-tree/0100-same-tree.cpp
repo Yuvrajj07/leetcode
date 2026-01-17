0ff85030d97e1c82dbf0e55eacc67e54a6db4ff3
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2026-01-17 22:46:37 +0530

    Time: 0 ms (100%), Space: 12.8 MB (76.52%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,18 +11,13 @@
  */
 class Solution {
 public:
+    bool solve(TreeNode* p, TreeNode* q){
+        if (p==NULL && q==NULL) return true;
+        else if (p==NULL || q==NULL) return false;
+        if (p->val!=q->val) return false;
+        return (solve (p->left,q->left) && solve (p->right,q->right));
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-    if (p==NULL && q==NULL){
-        return true;
+        return solve (p,q);
     }
-    if (p==NULL || q==NULL){
-        return false ;
-    }
-    if (p->val!=q->val){
-        return false;
-    }
-    return isSameTree(p->left,q->left) && isSameTree(p->right ,q->right);
-    }
-
-    
 };
