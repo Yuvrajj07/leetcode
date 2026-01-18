8a0d3985d6a5308ef013495d6a6e0361a70658dc
Yuvraj Singh <131692778+Yuvrajj07@users.noreply.github.com>
2026-01-18 23:51:28 +0530

    Time: 0 ms (100%), Space: 12.5 MB (95.81%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,31 @@
+// class Solution {
+// public:
+//     int solve (vector<vector<int>>&v,int i,int j,vector<vector<int>>&dp){
+//         // if (i>=v.size() ||  j>=v[i].size()) return 0;
+//         if (i == v.size() - 1) return v[i][j];
+//         if (dp[i][j]!=-1) return dp[i][j];
+//         int curr=0,n=0;
+//         curr=v[i][j]+solve(v,i+1,j,dp);
+//         n=v[i][j]+solve(v,i+1,j+1,dp);
+//         return dp[i][j]=min(curr,n);
+//     }
+//     int minimumTotal(vector<vector<int>>&v) {
+//         vector<vector<int>>dp(v.size(),(vector<int>(v[v.size()-1].size(),-1)));
+//         return solve (v,0,0,dp);
+//     }
+// };
+
+
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& t) {
-        int a,b;
-        a=t.size();
-        b=t[a-1].size();
-        vector<vector<int>>v(a,vector<int>(b,INT_MAX));
-        for(int i=0;i<a;i++)
-        {
-            for(int j=0;j<t[i].size();j++)
-            {
-                v[i][j]=t[i][j];
+    int minimumTotal(vector<vector<int>>& v) {
+        vector<int> dp = v.back();
+
+        for (int i = v.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < v[i].size(); j++) {
+                dp[j] = v[i][j] + min(dp[j], dp[j + 1]);
             }
         }
-        for(int i=a-2;i>=0;i--)
-        {
-            for(int j=0;j<t[i].size();j++)
-            {
-                v[i][j]=v[i][j]+min(v[i+1][j],v[i+1][j+1]);
-            }
-        }
-        return v[0][0];
+        return dp[0];
     }
 };
